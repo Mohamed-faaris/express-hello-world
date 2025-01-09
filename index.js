1812068cacf3097553c0272b88474a639faa35a8
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2025-01-09 14:06:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 80;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
