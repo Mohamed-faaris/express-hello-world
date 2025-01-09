ff838a39d03c086fd0e6be4c9a61da91b72bea51
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2025-01-09 14:21:08 +0530

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ express = require('express')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
